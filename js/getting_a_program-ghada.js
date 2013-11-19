8fb3e56483c31d0a61e1fb0926b9be3248399bdc
Ghada <ghada@cgc-online.com>
2013-11-19 13:31:47 +0100

    ghada committing program jsons files and testing a dropdown list to choose a single event without registration

---
+++ js/getting_a_program-ghada.js
@@ -0,0 +1,13 @@
+$("button").click(function()
+{
+document.write("vhvv");
+}
+<!--{
+  $.getJSON("C:/New phd stuff/INF5750/Json Files/program.json",function(result){
+alert("something wrong");
+    $.each(result, function(i, field){
+    $("ul").append("<li>"+field.name+"</li>");
+      });
+     
+  });
+});-->
