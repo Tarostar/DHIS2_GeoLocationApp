c3fbeff8829512d2df7d473d50426146ce8d1a40
S163488 <S163488@yahoo.com>
2013-11-24 13:50:33 +0100

    The changes that have been made are for the following purposes: - formjs.js: This js file contains methods that can get the json for   programs, and populate a form with data elements based on the selected   program. The only problem is that when a program is selected from a drop   down list that is populated dynmically, then the form that gets created   doesn't allways contain all the data elements in this particular   program. We will have to talke with studen assistant on Monday to get   help.

--- js/formjs.js
+++ js/formjs.js
@@ -1,15 +1,108 @@
+function populateProgramForm(data){
+	$.each(data.programs, function(index, val){
+		if(val.type == 3){
+		
+			$('#myProgramSelector').append($(document.createElement("option")).
+                        attr("value",val.href + ".json").text(val.name));
+						//alert(val.href + ".json");
+		}
+	});
+	//alert(htmlText);
 
-$(document).ready(function() {
-	  var json = [{"name":"Age", "type":"int"}, {"name":"Gender", "type":"string"}]; //, "type":"int"},{"name":"gender", "type":"string"}]';
-	  for (var i = 0; i < json.length; i++) {
-		  var name = json[i].name;
-		  var type = json[i].type;
-		  var htmltext = "<div class=\"form-group\">";
-		  htmltext += "<label for=\"input" + name + "\" class=\"col-sm-2 control-label\">" + name + "</label>";
-		  htmltext += "<div class=\"col-sm-4\">";
-		  htmltext += "<input type=\"" + type + "\" class=\"form-control\" id=\"input" + name + "\" placeholder=\"" + name + "\">";
-		  htmltext += "</div>";
-		  htmltext += "</div>";
+	//$('#bs3Select').empty();
+	//$("#bs3Select").append($("<option></option>").attr("value","1").text("hello"));
+	//$('#bs3Select').html(htmlText);
+	
+};
+var dataElementArray = [];
+function populateDataElementForm(){
+	$('#myProgramSelector').change(function(){
+		var selectedProgramURL = $('#myProgramSelector').val();
+		//alert('"'+selectedProgramURL+'"');
+		$.getJSON(selectedProgramURL, function(dataElementURL){
+			var programStageURL = dataElementURL.programStages[0].href + ".json";//dataElement.programStages.href;
+			//console.log(dataElementURL);
+				//alert(programStageURL);
+			$.getJSON(programStageURL, function(dataElement){
+				$.each(dataElement.programStageDataElements, function(index, val){
+					var typeURL = "http://localhost:8080/api/dataElements/" + val.dataElement.id + ".json";
+					var type = "";
+					$.getJSON(typeURL, function(dataElementType){
+						var elementWithType = {};
+						elementWithType["name"] = val.dataElement.name;
+						elementWithType["type"] = dataElementType.type;
+						//alert(elementWithType);
+						dataElementArray.push(elementWithType);
+						//console.log(index +' '+ dataElement.programStageDataElements.length);
+						if(index == (dataElement.programStageDataElements.length - 1))
+						{
+							console.log(dataElementArray);
+							createForm(dataElementArray);
+						}		//console.log(dataElementType);
+								//alert(val.dataElement.name + ": " + dataElementType.type);
+								
+					});
+					
+				});
+				//alert(dataElementArray[0]);
+				//createForm(dataElementArray);
+			});		
+
+		});
+		//createForm(dataElementArray);
+	});
+};
+
+function getCreateForm(){
+	//alert(
+};
+		/*$('#myProgramSelector').change(function(){
+alert("hi");
+//document.write("ggfgfg"+ $("#myProgramSelector").val() +"gfgfg");
+
+	var selectedProgramURL = $('#myProgramSelector').val();
+	alert(selectedProgramURL);
+	$.getJSON(selectedProgramURL, function(dataElement){
+		var programStageURL = dataElement.programStages.href;
+			alert(programStageURL);
+	});*/
+
+
+
+function getPrograms(){
+
+		//$.post("http://localhost:8080/dhis-web-commons/security/login.action",{j_username:"admin", j_password:"district"});
+	  $.getJSON("http://localhost:8080/api/programs.json", function(json) {
+		populateProgramForm(json);
+                        
+		});
+	
+};
+
+
+
+/*$(document).ready(function() {
+	$.post("http://apps.dhis2.org/demo/dhis-web-commons/security/login.action",{j_username:"admin", j_password:"district"});
+    alert("hi");
+    $.getJSON("http://apps.dhis2.org/demo/api/dataDictionaries.json", function(data) {
+    // Get the element with id summary and set the inner text to the result.
+    	var obj = data.cookie_status;
+       alert(obj);
+    $('#myForm').append(obj.[0]);
+});
+});*/
+
+function createForm(dataElementArray){//(dataElementsArray){
+	  //var json = [{"name":"Age", "type":"int"}, {"name":"Gender", "type":"string"}]; //, "type":"int"},{"name":"gender", "type":"string"}]';
+	  for (var i = 0; i < dataElementArray.length; i++) {
+		  var name = dataElementArray[i].name;
+		  var type = dataElementArray[i].type;
+		  var htmltext = '<div class="form-group">';
+		  htmltext += '<label for="input"' + name + ' class="col-sm-2 control-label">' + name + '</label>';
+		  htmltext += '<div class="col-sm-4">';
+		  htmltext += '<input type="' + type + '" class="form-control" id="input' + name + '" placeholder="' + name + '">';
+		  htmltext += '</div>';
+		  htmltext += '</div>';
 /*		  $('#myForm').append("<div class=\"form-group\">");
 		  $('#myForm').append("<label for=\"input" + name + "\" class=\"col-sm-2 control-label\">" + name + "</label>");
 		  $('#myForm').append("<div class=\"col-sm-4\">");
@@ -19,4 +112,6 @@ $(document).ready(function() {
 	*/
 		  $('#myForm').append(htmltext);
 	  };
-	});
+
+};
+
