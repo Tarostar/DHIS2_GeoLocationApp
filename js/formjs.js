a25b0db1ff9a0d87491a7d9547625f7d882eb533
S163488 <S163488@yahoo.com>
2013-11-27 09:35:07 +0100

    modified the formjs, index.html and form.html to create the form dynamically in the main page. There is still a problem with geeting the optionSets

--- js/formjs.js
+++ js/formjs.js
@@ -1,45 +1,19 @@
 function populateProgramForm(data){
+	//$('#programDIV').append($(document.createElement("select")).
+	//attr("value",val.href + ".json").text(val.name));
 	$.each(data.programs, function(index, val){
 		if(val.type == 3){
 		
-			// value: id / href
 			$('#myProgramSelector').append($(document.createElement("option")).
-                        attr("value",val.id + "/" + val.href + ".json").text(val.name));
+                        attr("value",val.href + ".json").text(val.name));
 						//alert(val.href + ".json");
 		}
 	});
-	//alert(htmlText);
-
-	//$('#bs3Select').empty();
-	//$("#bs3Select").append($("<option></option>").attr("value","1").text("hello"));
-	//$('#bs3Select').html(htmlText);
-	
 };
 var dataElementArray = [];
 function populateDataElementForm(){
 	$('#myProgramSelector').change(function(){
-		var programValue = $('#myProgramSelector').val();
-		// we know id is first part of value so extract second half
-		var n = programValue.indexOf("/");
-		if (n != -1) {
-			selectedProgramURL = programValue.substring(n + 1);
-			alert(selectedProgramURL);
-			
-			if (Modernizr.localstorage) {
-				localStorage["programID"] = programValue.substring(0, n - 1)
-			} else {
-				// TODO: consider alternatives that do not rely on local storage
-				// no native support for HTML5 storage :(
-				// maybe try dojox.storage or a third-party solution
-				alert("local storage not supported, this app requires HTML5 localStorage support")
-			}
-	    }
-		else
-		{
-			// TODO: better error handling
-				alert("this should never happen, code broken!");
-		}
-		
+		var selectedProgramURL = $('#myProgramSelector').val();
 		//alert('"'+selectedProgramURL+'"');
 		$.getJSON(selectedProgramURL, function(dataElementURL){
 			var programStageURL = dataElementURL.programStages[0].href + ".json";//dataElement.programStages.href;
@@ -47,21 +21,43 @@ function populateDataElementForm(){
 				//alert(programStageURL);
 			$.getJSON(programStageURL, function(dataElement){
 				$.each(dataElement.programStageDataElements, function(index, val){
-					var typeURL = dhisAPI + "/api/dataElements/" + val.dataElement.id + ".json";
+					var typeURL = "http://localhost:8080/api/dataElements/" + val.dataElement.id + ".json";
 					var type = "";
 					$.getJSON(typeURL, function(dataElementType){
 						var elementWithType = {};
 						elementWithType["name"] = val.dataElement.name;
 						elementWithType["type"] = dataElementType.type;
+/* 						if (dataElementType.optionSet != null)
+						{
+						
+							var optionSetUrl = dataElementType.optionSet.href + ".json";//
+							
+							var optionSetArray = [];
+							$.getJSON(optionSetUrl, function(dataoption){
+/* 								$.each(dataoption.options, function(index, val){
+								//alert(val);
+									optionSetArray.push(val);
+								}); */
+								/*var str = dataoption.options;
+						elementWithType["options"] = str.split(',');
+						alert(elementWithType["options"] );
+							});
+
+								
+
+						} */
+						
 						//alert(elementWithType);
 						dataElementArray.push(elementWithType);
 						//console.log(index +' '+ dataElement.programStageDataElements.length);
 						if(index == (dataElement.programStageDataElements.length - 1))
 						{
 							console.log(dataElementArray);
+						//document.write("hellooooo");
 							createForm(dataElementArray);
 						}		//console.log(dataElementType);
 								//alert(val.dataElement.name + ": " + dataElementType.type);
+								
 								
 					});
 					
@@ -94,7 +90,7 @@ alert("hi");
 function getPrograms(){
 
 		//$.post("http://localhost:8080/dhis-web-commons/security/login.action",{j_username:"admin", j_password:"district"});
-	  $.getJSON(dhisAPI + "/api/programs.json", function(json) {
+	  $.getJSON("http://localhost:8080/api/programs.json", function(json) {
 		populateProgramForm(json);
                         
 		});
@@ -115,14 +111,41 @@ function getPrograms(){
 });*/
 
 function createForm(dataElementArray){//(dataElementsArray){
-	  //var json = [{"name":"Age", "type":"int"}, {"name":"Gender", "type":"string"}]; //, "type":"int"},{"name":"gender", "type":"string"}]';
+	  //var json = [{"name":"Age", "type":"int"}, {"name":"Gender", "type":"string", "options":["Male","Female","N/A"]}]; //, "type":"int"},{"name":"gender", "type":"string"}]';
 	  for (var i = 0; i < dataElementArray.length; i++) {
+	   //for (var j = 0; j < dataElementArray.length; j++)
+	  
 		  var name = dataElementArray[i].name;
+		 // alert( dataElementArray[i].name);
 		  var type = dataElementArray[i].type;
+		  		  //alert( dataElementArray[i].options);
+
+/* 		  var option_menu = new Array();
+ 		  if (dataElementArray[i].length = 3)
+		  {
+		  
+				 option_menu  = dataElementArray[i].options;
+				 alert( dataElementArray[i].options);
+				for (i=0; i<= option_menu.length; i++)
+				alert(option_menu[i]);
+		  } */
+		  
+		  
 		  var htmltext = '<div class="form-group">';
 		  htmltext += '<label for="input"' + name + ' class="col-sm-2 control-label">' + name + '</label>';
 		  htmltext += '<div class="col-sm-4">';
-		  htmltext += '<input type="' + type + '" class="form-control" id="input' + name + '" placeholder="' + name + '">';
+ 		 /* if(options.length != 0)
+		  {  
+		  htmltext += '<select onChange>';
+		  $.each(options, function( val){
+				htmltext += '  <option value="def" >' + options.val +'</option>';
+		  });
+		  htmltext += '</select>';
+		   }else{  */
+					  htmltext += '<input type="' + type + '" class="form-control" id="input' + name + '" placeholder="' + name + '">';
+
+		  //}
+		  
 		  htmltext += '</div>';
 		  htmltext += '</div>';
 /*		  $('#myForm').append("<div class=\"form-group\">");
@@ -132,6 +155,7 @@ function createForm(dataElementArray){//(dataElementsArray){
 	      $('#myForm').append("</div>");
 		  $('#myForm').append("</div>");
 	*/
+	alert(htmltext);
 		  $('#myForm').append(htmltext);
 	  };
 
